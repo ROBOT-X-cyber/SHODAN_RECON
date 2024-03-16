1e037a70ad59fe61ab44dead3974d92539519757
Abhirup Konwar <80304057+ROBOT-X-cyber@users.noreply.github.com>
2024-03-16 11:14:00 +0530

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,7 +1,9 @@
-SHODAN DORK
+#SHODAN DORK
 org:"Organisation name here"
 
-Find the organisation name from SSL Certificate.
+#Find the organisation name from SSL Certificate.
+
+#Perform facet analysis for IP 
 
 #save the page source in a file named shodan.html
 cat shodan.html| grep -o '<strong>.*</strong>' | sed 's/<strong>\(.*\)<\/strong>/\1/g' > ips 
